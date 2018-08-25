dc678d84955a95be3c53beb9dc345218342dc885
Daniel Vigh <vighd@digital.co.hu>
2018-08-25 22:10:40 +0200

    Remove monocle window number count

--- dwm.c
+++ dwm.c
@@ -1124,10 +1124,7 @@ monocle(Monitor *m)
 	Client *c;
 
 	for (c = m->clients; c; c = c->next)
-		if (ISVISIBLE(c))
-			n++;
-	if (n > 0) /* override layout symbol */
-		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
+		if (ISVISIBLE(c))n++;
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
 		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
 }
